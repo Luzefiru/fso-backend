dd37d79e2b7fd8b1790ef8056959cabfc8ad5b4c
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-11 11:36:11 +0800

    feat: finish 3.20

--- models/entry.js
+++ models/entry.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 
+function validatePhoneNumber(inp) {
+  const regex = /^\d{2,3}-\d+$/;
+  return regex.test(inp);
+}
+
 const EntrySchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: validatePhoneNumber,
+      message: (props) => `${props.value} is not a valid Phone Number`,
+    },
+    required: true,
+  },
 });
 
 EntrySchema.set('toJSON', {
